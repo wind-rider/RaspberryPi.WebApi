d6e6e37cbabd2f7cbc598255932fe549d234b1d2
wind-rider <hansmbakker+github@gmail.com>
2017-04-27 20:25:46 +0200

    More efficient deployment

--- RaspberryPi.WebApi/piwebapi/deploy-windows.ps1
+++ RaspberryPi.WebApi/piwebapi/deploy-windows.ps1
@@ -2,4 +2,5 @@ param ([string]$ip, [string]$destination)
 
 & ".\publish-windows.ps1"
 
-& xcopy.exe /y ".\bin\Debug\netcoreapp2.0\win8-arm\publish" "\\$ip\$destination"
+# only copy newer files; remove files that were deleted in the source folder
+& robocopy.exe /MIR ".\bin\Debug\netcoreapp2.0\win8-arm\publish" "\\$ip\$destination"
